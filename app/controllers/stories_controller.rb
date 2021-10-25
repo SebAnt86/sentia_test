23a073e1b849d0ed27e11f25382b8652b47b2d4a
Antonio Sebastiano <sebastianoa@hotmail.com>
2021-10-25 14:10:57 +1100

    create form import update config and controller

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -2,4 +2,11 @@ class StoriesController < ApplicationController
   def index
     @stories = Story.all
   end
+
+  def import
+    Story.import(params[:file])
+    redirect_to stories_path, notice: "Stories Added Successfully!"
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :stories do 
+    colloection { post :import}
+  end
+
   root "stories#index"
 
   get "/stories", to: "stories#index"
